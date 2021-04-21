77ce8430b0a00b6078e53ca6e9c1796b49f6c521
gianluca <g.gargaro@gmail.com>
2021-04-21 18:39:49 +0200

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -10,15 +10,15 @@
 resource ibm_container_cluster kubernetes_cluster {
 
     name              = var.cluster_name
-    datacenter        = local.master.zone
+    datacenter        = var.zone
     machine_type      = var.machine_type
     hardware          = var.hardware
     kube_version      = var.kube_version
-    public_vlan_id    = local.master.public_vlan_id
-    private_vlan_id   = local.master.private_vlan_id
+    public_vlan_id    = var.public_vlan_id
+    private_vlan_id   = var.private_vlan_id
     default_pool_size = var.default_pool_size
-    public_service_endpoint  = "true"
-    private_service_endpoint = "true"
+    public_service_endpoint  = var.public_endpoint
+    private_service_endpoint = var.private_endpoint
 }
 
 #############################################################################
