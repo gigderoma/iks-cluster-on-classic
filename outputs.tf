f9af1ec3f29d3f76b71543999d7cf53ec2ba99c2
gianluca <g.gargaro@gmail.com>
2021-04-21 18:21:58 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,6 @@
 output cluster_id {
     description = "Cluster ID, will await multizone attachment before returning"
     value       = ibm_container_cluster.openshift_cluster.id
-    depends_on  = [ibm_container_worker_pool_zone_attachment.multi_zone]
 }
 
 #############################################################################
